7b44db797ec8b9ff03b8eda98a6a85261a366fd2
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-09-27 16:34:29 +0100

    some renaming

--- src/dataloaders/dataloader.py
+++ src/dataloaders/dataloader.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pathlib
 import torch
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader as TorchDataLoader, Subset
 from typing import *
 
 from ..utils.log import get_logger
@@ -41,10 +41,12 @@ class Dataloader:
             start_idx = 0
             for purpose in split.keys():
                 len_subset = int(split[purpose] * len(dataset))
-                subset_indices = indices[start_idx:start_idx+len_subset]
+                subset_indices = indices[start_idx:start_idx + len_subset]
                 subsets[purpose] = Subset(dataset, subset_indices)
 
                 start_idx += len_subset
+
+        print(subsets)
 
         self.dataloaders = {}
         for purpose in ["train", "val", "test"]:
@@ -53,10 +55,10 @@ class Dataloader:
             else:
                 dataset = subsets[purpose]
 
-            self.dataloaders[purpose] = DataLoader(dataset=dataset,
-                                                   batch_size=self.config["batch_size"],
-                                                   shuffle=self.config.get("shuffle", True),
-                                                   num_workers=self.config["num_workers"])
+            self.dataloaders[purpose] = TorchDataLoader(dataset=dataset,
+                                                        batch_size=self.config["batch_size"],
+                                                        shuffle=self.config.get("shuffle", True),
+                                                        num_workers=self.config["num_workers"])
 
     def __str__(self):
         return str(self.config)
