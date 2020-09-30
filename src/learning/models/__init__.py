cf8328dd0805d5fe3af4e04d5150e3eb66f89a61
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-09-30 11:15:43 +0100

    preliminary implementation of nvidia partialconv paper inpainting

--- src/learning/models/__init__.py
+++ src/learning/models/__init__.py
@@ -1,5 +1,6 @@
 from .base_model import *
 from .baseline.open_cv_baseline import OpenCVBaseline
+from .partialconv.partialconv_unet import PartialConvUNet
 from .vae.base_vae import BaseVAE
 from .vae.vanilla_vae import VanillaVAE
 from .vae.vq_vae import VQVAE
@@ -8,6 +9,7 @@ from .unet.unet_model import UNet
 MODELS = {"VanillaVAE": VanillaVAE,
           "NavierStokes": OpenCVBaseline,
           "Telea": OpenCVBaseline,
+          "PartialConvUNet": PartialConvUNet,
           "PatchMatch": OpenCVBaseline,
           "VQVAE": VQVAE,
           "UNet": UNet}
