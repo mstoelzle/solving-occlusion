2b727948e043e07cec4c2c87b3b2f0d2f7ee25ce
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-22 17:40:26 +0200

    added apply_to config to transforms to specify channels

--- src/datasets/transforms.py
+++ src/datasets/transforms.py
@@ -11,27 +11,30 @@ class Transformer:
         self.transforms = transforms
         self.rng = np.random.RandomState(seed=1)
 
+        self.deterministic = False
+        if self.purpose == "test":
+            self.deterministic = True
+
     def __call__(self, input: torch.Tensor, channel: ChannelEnum = None) -> torch.Tensor:
         transformed_input = input
 
         for transform_config in self.transforms:
             if transform_config["type"] == "random_noise":
                 transformed_input = self.random_noise(transform_config, transformed_input, channel=channel)
-            elif transform_config["type"] == "random_scale":
-                transformed_input = self.random_scale(transform_config, transformed_input, channel=channel)
-            elif transform_config["type"] == "random_offset":
-                transformed_input = self.random_offset(transform_config, transformed_input, channel=channel)
+            elif transform_config["type"] == "random_vertical_scale":
+                transformed_input = self.random_vertical_scale(transform_config, transformed_input, channel=channel)
+            elif transform_config["type"] == "random_vertical_offset":
+                transformed_input = self.random_vertical_offset(transform_config, transformed_input, channel=channel)
             else:
                 raise NotImplementedError
 
         return transformed_input
 
     def random_noise(self, transform_config: dict, input: np.array, channel: ChannelEnum = None) -> np.array:
-        if channel in [None, ChannelEnum.OCCLUDED_ELEVATION_MAP]:
+        if channel is None or channel.value in transform_config["apply_to"]:
             stdev = transform_config["stdev"]
 
-            deterministic = transform_config.get("deterministic", True)
-            if deterministic:
+            if self.deterministic:
                 noise = self.rng.normal(loc=0, scale=stdev, size=tuple(input.size()))
             else:
                 noise = np.random.normal(loc=0, scale=stdev, size=tuple(input.size()))
@@ -43,32 +46,40 @@ class Transformer:
 
         return input
 
-    def random_scale(self, transform_config: dict, input: np.array, channel: ChannelEnum = None) -> np.array:
-        if channel in [None, ChannelEnum.OCCLUDED_ELEVATION_MAP, ChannelEnum.GROUND_TRUTH_ELEVATION_MAP]:
+    def random_vertical_scale(self, transform_config: dict, input: np.array, channel: ChannelEnum = None) -> np.array:
+        if channel is None or channel.value in transform_config["apply_to"]:
             min, max = transform_config["min"], transform_config["max"]
 
-            deterministic = transform_config.get("deterministic", True)
-            if deterministic:
+            if self.deterministic:
                 scale = self.rng.uniform(low=min, high=max)
             else:
                 scale = np.random.uniform(low=min, high=max)
 
-            transformed_input = scale * input
+            if channel is ChannelEnum.PARAMS:
+                transformed_input = input.clone()
+                transformed_input[3] = scale * transformed_input[3]
+            else:
+                transformed_input = scale * input
+
             return transformed_input
 
         return input
 
-    def random_offset(self, transform_config: dict, input: np.array, channel: ChannelEnum = None) -> np.array:
-        if channel in [None, ChannelEnum.OCCLUDED_ELEVATION_MAP, ChannelEnum.GROUND_TRUTH_ELEVATION_MAP]:
+    def random_vertical_offset(self, transform_config: dict, input: np.array, channel: ChannelEnum = None) -> np.array:
+        if channel is None or channel.value in transform_config["apply_to"]:
             min, max = transform_config["min"], transform_config["max"]
 
-            deterministic = transform_config.get("deterministic", True)
-            if deterministic:
+            if self.deterministic:
                 offset = self.rng.uniform(low=min, high=max)
             else:
                 offset = np.random.uniform(low=min, high=max)
 
-            transformed_input = offset + input
+            if channel is ChannelEnum.PARAMS:
+                transformed_input = input.clone()
+                transformed_input[3] = offset + transformed_input[3]
+            else:
+                transformed_input = offset + input
+
             return transformed_input
 
         return input
