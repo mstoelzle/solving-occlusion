67ab55354ebc53fd4476b6a30f4dbe7ea26d977e
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-22 13:19:13 +0200

    added random_offset and random_scale transforms

--- src/datasets/transforms.py
+++ src/datasets/transforms.py
@@ -14,16 +14,53 @@ class Transformer:
 
         for transform in self.transforms:
             if transform["type"] == "random_noise":
-                stdev = transform["stdev"]
+                transformed_input = self.random_noise(transform, transformed_input)
+            elif transform["type"] == "random_scale":
+                transformed_input = self.random_scale(transform, transformed_input)
+            elif transform["type"] == "random_offset":
+                transformed_input = self.random_offset(transform, transformed_input)
+            else:
+                raise NotImplementedError
 
-                deterministic = transform.get("deterministic", True)
-                if deterministic:
-                    noise = self.rng.normal(loc=0, scale=stdev, size=tuple(input.size()))
-                else:
-                    noise = np.random.normal(loc=0, scale=stdev, size=tuple(input.size()))
+        return transformed_input
 
-                noise = input.new_tensor(noise, dtype=input.dtype)
+    def random_noise(self, transform: dict, input: np.array) -> np.array:
+        stdev = transform["stdev"]
 
-                transformed_input = transformed_input + noise
+        deterministic = transform.get("deterministic", True)
+        if deterministic:
+            noise = self.rng.normal(loc=0, scale=stdev, size=tuple(input.size()))
+        else:
+            noise = np.random.normal(loc=0, scale=stdev, size=tuple(input.size()))
+
+        noise = input.new_tensor(noise, dtype=input.dtype)
+
+        transformed_input = input + noise
+
+        return transformed_input
+
+    def random_scale(self, transform: dict, input: np.array) -> np.array:
+        min, max = transform["min"], transform["max"]
+
+        deterministic = transform.get("deterministic", True)
+        if deterministic:
+            scale = self.rng.uniform(low=min, high=max)
+        else:
+            scale = np.random.uniform(low=min, high=max)
+
+        transformed_input = scale * input
+
+        return transformed_input
+
+    def random_offset(self, transform: dict, input: np.array) -> np.array:
+        min, max = transform["min"], transform["max"]
+
+        deterministic = transform.get("deterministic", True)
+        if deterministic:
+            offset = self.rng.uniform(low=min, high=max)
+        else:
+            offset = np.random.uniform(low=min, high=max)
+
+        transformed_input = offset + input
 
         return transformed_input
