3431886b8d1c0a9990f0e9f300b367e5fd8a6ee2
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-23 21:28:52 +0200

    added division of grids from rosbag into subgrids to AnyboticsRosbagDatasetGenerator

--- src/dataset_generation/anybotics_rosbag_dataset_generator.py
+++ src/dataset_generation/anybotics_rosbag_dataset_generator.py
@@ -21,6 +21,17 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
         self.reset()
 
     def reset(self):
+        self.reset_cache()
+
+        self.initialized_datasets = False
+        self.hdf5_group = None
+        self.params_dataset = None
+        self.occluded_elevation_map_dataset = None
+
+        self.sample_idx = 0
+        self.max_num_samples = None
+
+    def reset_cache(self):
         self.params = []
         self.occluded_elevation_maps = []
 
@@ -38,18 +49,12 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
         # TODO: maybe we need to add additional purposes later
         purpose = "test"
 
-        topics = ['/elevation_mapping/elevation_map_recordable']
-        num_samples = self.bag.get_message_count(topic_filters=topics)
+        self.rosbag_topics = ['/elevation_mapping/elevation_map_recordable']
 
-        hdf5_group = self.hdf5_file.create_group(purpose)
+        self.hdf5_group = self.hdf5_file.create_group(purpose)
 
-        params_dataset = None
-        occluded_elevation_map_dataset = None
-
-        progress_bar = Bar(f"Reading {purpose} anybotics bag", max=num_samples)
-        initialized_hdf5_datasets = False
-        sample_idx = 0
-        for msg_idx, (topic, msg, t) in enumerate(self.bag.read_messages(topics=topics)):
+        progress_bar = None
+        for msg_idx, (topic, msg, t) in enumerate(self.bag.read_messages(topics=self.rosbag_topics)):
             for layer_idx in range(len(msg.layers)):
                 info = msg.info
                 layer = msg.layers[layer_idx]
@@ -62,7 +67,6 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
                 position = np.array([pose.position.x, pose.position.y, pose.position.z])
                 orientation = np.array([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
                 yaw = Rotation.from_quat(orientation).as_euler('zyx')[2]
-                params = np.array([resolution, position[0], position[1], position[2], yaw])
 
                 layout = layer_data.layout
                 dims = layout.dim
@@ -81,27 +85,68 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
                 else:
                     raise ValueError
 
-                self.params.append(params)
-                self.occluded_elevation_maps.append(grid_map)
+                target_size_x = self.config.get("size", grid_map.shape[0])
+                target_size_y = self.config.get("size", grid_map.shape[1])
+                num_subgrids_x = int(np.floor(grid_map.shape[0] / target_size_x))
+                num_subgrids_y = int(np.floor(grid_map.shape[0] / target_size_y))
 
-                if initialized_hdf5_datasets is False:
-                    dataset_shape = (0, grid_map.shape[0], grid_map.shape[1])
-                    dataset_maxshape = (num_samples, grid_map.shape[0], grid_map.shape[1])
-                    params_dataset = hdf5_group.create_dataset(name=ChannelEnum.PARAMS.value,
-                                                               shape=(0, params.shape[0]),
-                                                               maxshape=(num_samples, params.shape[0]))
-                    occluded_elevation_map_dataset = hdf5_group.create_dataset(name=ChannelEnum.OCCLUDED_ELEVATION_MAP.value,
-                                                                               shape=dataset_shape,
-                                                                               maxshape=dataset_maxshape)
-                    initialized_hdf5_datasets = True
+                if self.max_num_samples is None:
+                    self.max_num_samples = self.bag.get_message_count(topic_filters=self.rosbag_topics)
+                    self.max_num_samples *= num_subgrids_x * num_subgrids_y
+                    progress_bar = Bar(f"Reading anybotics bag", max=self.max_num_samples)
 
-                if sample_idx % self.config.get("save_frequency", 50) == 0 or \
-                        sample_idx >= num_samples:
-                    self.extend_dataset(params_dataset, self.params)
-                    self.extend_dataset(occluded_elevation_map_dataset, self.occluded_elevation_maps)
-                    self.reset()
+                start_x = 0
+                start_y = 0
+                for i in range(num_subgrids_x):
+                    stop_x = start_x + target_size_x
+                    start_y = 0
+                    for j in range(num_subgrids_y):
+                        stop_y = start_y + target_size_y
 
-                sample_idx += 1
+                        subgrid = grid_map[start_x:stop_x, start_y:stop_y]
 
-            progress_bar.next()
+                        if np.isnan(subgrid).sum() > (target_size_x * target_size_y / 2):
+                            # we do not want to include the subgrid in the dataset if its occluded to more than 50%
+                            progress_bar.next()
+                            start_y = stop_y
+                            continue
+
+                        subgrid_delta_x = resolution * (-grid_map.shape[0]/2 + start_x + target_size_x / 2)
+                        subgrid_delta_y = resolution * (-grid_map.shape[1]/2 + start_y + target_size_y / 2)
+                        params = np.array([resolution, position[0] + subgrid_delta_x, position[1] + subgrid_delta_y,
+                                           position[2], yaw])
+
+                        self.process_item(params, subgrid)
+
+                        self.sample_idx += 1
+                        progress_bar.next()
+                        start_y = stop_y
+
+                    start_x = stop_x
+
+        self.extend_dataset(self.params_dataset, self.params)
+        self.extend_dataset(self.occluded_elevation_map_dataset, self.occluded_elevation_maps)
+        self.reset()
+
         progress_bar.finish()
+
+    def process_item(self, params: np.array, subgrid: np.array):
+        self.params.append(params)
+        self.occluded_elevation_maps.append(subgrid)
+
+        if self.initialized_datasets is False:
+            dataset_shape = (0, subgrid.shape[0], subgrid.shape[1])
+            dataset_maxshape = (self.max_num_samples, subgrid.shape[0], subgrid.shape[1])
+            self.params_dataset = self.hdf5_group.create_dataset(name=ChannelEnum.PARAMS.value,
+                                                                 shape=(0, params.shape[0]),
+                                                                 maxshape=(self.max_num_samples, params.shape[0]))
+            self.occluded_elevation_map_dataset = \
+                self.hdf5_group.create_dataset(name=ChannelEnum.OCCLUDED_ELEVATION_MAP.value,
+                                               shape=dataset_shape, maxshape=dataset_maxshape)
+            self.initialized_datasets = True
+
+        if self.sample_idx % self.config.get("save_frequency", 50) == 0 or \
+                self.sample_idx >= self.max_num_samples:
+            self.extend_dataset(self.params_dataset, self.params)
+            self.extend_dataset(self.occluded_elevation_map_dataset, self.occluded_elevation_maps)
+            self.reset_cache()
