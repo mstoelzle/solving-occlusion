d8d11642e0ec50fb3c4d93e9549d951583e6b240
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-11-30 22:24:31 +0000

    added purposes to anymal_rosbag_stairs dataset

--- src/dataset_generation/anybotics_rosbag_dataset_generator.py
+++ src/dataset_generation/anybotics_rosbag_dataset_generator.py
@@ -4,6 +4,7 @@ import numpy as np
 import pathlib
 from progress.bar import Bar
 from scipy.spatial.transform import Rotation
+from typing import *
 
 from .base_dataset_generator import BaseDatasetGenerator
 from src.enums import *
@@ -16,6 +17,11 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
         import rosbag
         self.rosbag_module = rosbag
         self.rosbag_path = pathlib.Path(self.config["rosbag_path"])
+        self.rosbag_topics = ['/elevation_mapping/elevation_map_recordable']
+
+        self.split_config = self.config.get("split")
+        assert list(self.split_config.keys()) == ["train", "val", "test"]
+        self.split_msg_indices = {}
 
         self.bag = None
         self.reset()
@@ -24,13 +30,14 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
         super().reset()
         self.reset_cache()
 
-        self.initialized_datasets = False
+        self.purpose = None
         self.hdf5_group = None
         self.params_dataset = None
         self.occluded_elevation_map_dataset = None
 
         self.sample_idx = 0
-        self.max_num_samples = None
+        self.total_num_samples = None
+        self.purpose_max_num_samples = {}
 
     def reset_cache(self):
         self.params = []
@@ -47,13 +54,6 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
         super().__exit__(exc_type, exc_val, exc_tb)
 
     def run(self):
-        # TODO: maybe we need to add additional purposes later
-        purpose = "test"
-
-        self.rosbag_topics = ['/elevation_mapping/elevation_map_recordable']
-
-        self.hdf5_group = self.hdf5_file.create_group(purpose)
-
         progress_bar = None
         for msg_idx, (topic, msg, t) in enumerate(self.bag.read_messages(topics=self.rosbag_topics)):
             for layer_idx in range(len(msg.layers)):
@@ -93,10 +93,36 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
                 num_subgrids_x = int(np.floor(grid_map.shape[0] / target_size_x))
                 num_subgrids_y = int(np.floor(grid_map.shape[0] / target_size_y))
 
-                if self.max_num_samples is None:
-                    self.max_num_samples = self.bag.get_message_count(topic_filters=self.rosbag_topics)
-                    self.max_num_samples *= num_subgrids_x * num_subgrids_y
-                    progress_bar = Bar(f"Reading anymal bag", max=self.max_num_samples)
+                if self.total_num_samples is None:
+                    total_num_messages = self.bag.get_message_count(topic_filters=self.rosbag_topics)
+
+                    total_split = 0
+                    for purpose, purpose_split in self.split_config.items():
+                        total_split += purpose_split
+                    start_msg_idx = 0
+                    for purpose, purpose_split in self.split_config.items():
+                        self.split_msg_indices[purpose] = start_msg_idx
+                        purpose_num_msgs = int(purpose_split / total_split * total_num_messages)
+                        self.purpose_max_num_samples[purpose] = purpose_num_msgs * num_subgrids_x * num_subgrids_y
+                        start_msg_idx += purpose_num_msgs
+
+                    self.total_num_samples = total_num_messages * num_subgrids_x * num_subgrids_y
+
+                    progress_bar = Bar(f"Reading anymal bag", max=self.total_num_samples)
+
+                if self.purpose is None and msg_idx >= self.split_msg_indices["train"]:
+                    self.purpose = "train"
+                    self.hdf5_group = self.hdf5_file.create_group(self.purpose)
+                elif self.purpose == "train" and msg_idx >= self.split_msg_indices["val"]:
+                    self.save_cache()
+                    self.purpose = "val"
+                    self.hdf5_group = self.hdf5_file.create_group(self.purpose)
+                elif self.purpose == "val" and msg_idx >= self.split_msg_indices["test"]:
+                    self.save_cache()
+                    self.purpose = "test"
+                    self.hdf5_group = self.hdf5_file.create_group(self.purpose)
+                else:
+                    pass
 
                 start_x = 0
                 start_y = 0
@@ -134,23 +160,27 @@ class AnyboticsRosbagDatasetGenerator(BaseDatasetGenerator):
 
         progress_bar.finish()
 
-    def process_item(self, params: np.array, subgrid: np.array):
+    def process_item(self, params: np.array, subgrid: np.array, force_save: bool = False):
         self.params.append(params)
         self.occluded_elevation_maps.append(subgrid)
 
-        if self.initialized_datasets is False:
+        if ChannelEnum.PARAMS.value not in self.hdf5_group or \
+                ChannelEnum.OCCLUDED_ELEVATION_MAP.value not in self.hdf5_group:
             dataset_shape = (0, subgrid.shape[0], subgrid.shape[1])
-            dataset_maxshape = (self.max_num_samples, subgrid.shape[0], subgrid.shape[1])
+            dataset_maxshape = (self.purpose_max_num_samples[self.purpose], subgrid.shape[0], subgrid.shape[1])
             self.params_dataset = self.hdf5_group.create_dataset(name=ChannelEnum.PARAMS.value,
                                                                  shape=(0, params.shape[0]),
-                                                                 maxshape=(self.max_num_samples, params.shape[0]))
+                                                                 maxshape=(self.purpose_max_num_samples[self.purpose],
+                                                                           params.shape[0]))
             self.occluded_elevation_map_dataset = \
                 self.hdf5_group.create_dataset(name=ChannelEnum.OCCLUDED_ELEVATION_MAP.value,
                                                shape=dataset_shape, maxshape=dataset_maxshape)
-            self.initialized_datasets = True
 
         if self.sample_idx % self.config.get("save_frequency", 50) == 0 or \
-                self.sample_idx >= self.max_num_samples:
-            self.extend_dataset(self.params_dataset, self.params)
-            self.extend_dataset(self.occluded_elevation_map_dataset, self.occluded_elevation_maps)
-            self.reset_cache()
+                self.sample_idx >= self.total_num_samples or force_save is True:
+            self.save_cache()
+
+    def save_cache(self):
+        self.extend_dataset(self.params_dataset, self.params)
+        self.extend_dataset(self.occluded_elevation_map_dataset, self.occluded_elevation_maps)
+        self.reset_cache()
