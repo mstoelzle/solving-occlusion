4ceae6d3d72dea90e4e7751e3c8202ab41b60797
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-12-02 21:53:17 +0000

    added range_adjusted_white_noise

--- src/datasets/transforms.py
+++ src/datasets/transforms.py
@@ -46,16 +46,38 @@ class Transformer:
                 data[channel] = transformed_value
 
         return data
+
+    def range_adjusted_white_noise(self, transform_config: dict,
+                                   data: Dict[ChannelEnum, torch.Tensor]) -> Dict[ChannelEnum, torch.Tensor]:
+        params = data[ChannelEnum.PARAMS]
+        terrain_resolution = params[0].item()
+        robot_position_x = params[1].item()
+        robot_position_y = params[2].item()
+
+        sample_grid = data[ChannelEnum.OCCLUDED_ELEVATION_MAP]
+
+        # distance of every pixel from the robot
+        lin_x = np.arange(start=-sample_grid.shape[0] / 2, stop=sample_grid.shape[0] / 2, step=1) * terrain_resolution
+        lin_y = np.arange(start=-sample_grid.shape[1] / 2, stop=sample_grid.shape[1] / 2, step=1) * terrain_resolution
+        off_x, off_y = np.meshgrid(lin_x, lin_y)
+
+        dist_x = robot_position_x - off_x
+        dist_y = robot_position_y - off_y
+        dist_p2_norm = np.sqrt(np.square(dist_x) + np.square(dist_y))
+
         stdev = transform_config["stdev"]
+        range = transform_config["range"]
+
+        scale = stdev * np.square(1 / range * dist_p2_norm)
 
         noise = None
         for channel, value in data.items():
             if channel.value in transform_config["apply_to"]:
                 if noise is None:
                     if self.deterministic:
-                        noise_value = self.rng.normal(loc=0, scale=stdev, size=tuple(value.size()))
+                        noise_value = self.rng.normal(loc=0, scale=scale, size=tuple(value.size()))
                     else:
-                        noise_value = np.random.normal(loc=0, scale=stdev, size=tuple(value.size()))
+                        noise_value = np.random.normal(loc=0, scale=scale, size=tuple(value.size()))
                     noise = value.new_tensor(noise_value, dtype=value.dtype)
 
                 transformed_value = value + noise
