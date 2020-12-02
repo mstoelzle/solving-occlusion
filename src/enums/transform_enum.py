0f705d9e8f4c7ca00845989387c574ddf0bd2839
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-12-02 21:28:35 +0000

    fixed bug in last commit

--- src/enums/transform_enum.py
+++ src/enums/transform_enum.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class TransformEnum(Enum):
-    RANDOM_NOISE = "random_noise"
+    WHITE_NOISE = "white_noise"
     RANDOM_VERTICAL_SCALE = "random_vertical_scale"
     RANDOM_VERTICAL_OFFSET = "random_vertical_offset"
     RANDOM_OCCLUSION = "random_occlusion"
