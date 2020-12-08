50f92b8b158a126120b4a32952849d9001f11420
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-12-08 22:46:24 +0000

    added uncertainty channels

--- src/enums/channel_enum.py
+++ src/enums/channel_enum.py
@@ -8,3 +8,6 @@ class ChannelEnum(Enum):
     OCCLUDED_ELEVATION_MAP = "occluded_elevation_map"
     BINARY_OCCLUSION_MAP = "binary_occlusion_map"
     COMPOSED_ELEVATION_MAP = "composed_elevation_map"
+    DATA_UNCERTAINTY_MAP = "data_uncertainty_map"
+    MODEL_UNCERTAINTY_MAP = "model_uncertainty_map"
+    TRAVERSABILITY_RISK_MAP = "traversability_risk_map"
