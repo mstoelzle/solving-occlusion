066027fe583fb2a75cc5d6e92efcb3349842ec1c
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-12-10 21:55:55 +0000

    make name of traversability risk map shorter

--- src/enums/channel_enum.py
+++ src/enums/channel_enum.py
@@ -13,4 +13,4 @@ class ChannelEnum(Enum):
     DATA_UNCERTAINTY_MAP = "data_uncertainty_map"
     MODEL_UNCERTAINTY_MAP = "model_uncertainty_map"
     TOTAL_UNCERTAINTY_MAP = "total_uncertainty_map"
-    TRAVERSABILITY_RISK_MAP = "traversability_risk_map"
+    TRAV_RISK_MAP = "trav_risk_map"
