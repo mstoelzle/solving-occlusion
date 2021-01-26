8e94ee1b34ac19f7bb5d2f5e50bad65740271129
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-01-26 11:57:59 +0100

    Introduce subgrids for ga_slam_msgpack_dataset_generator

--- src/dataset_generation/ga_slam_msgpack_dataset_generator.py
+++ src/dataset_generation/ga_slam_msgpack_dataset_generator.py
@@ -57,24 +57,10 @@ class GASlamMsgpackDatasetGenerator(BaseDatasetGenerator):
             gt_dem_msgs = chunk["/ga_slam.globalElevationMapMean"]
             gt_data_um_msgs = chunk["/ga_slam.globalElevationMapVariance"]
 
-            if progress_bar is None:
-                # we extrapolate the total maximum number of samples
-                # by comparing the number of messages and size of the current chunk
-                # TODO: I am not sure if this code is correct (for multiple chunks)
-                file_size = os.path.getsize(self.config["msgpack_path"])  # in bytes
-                self.total_num_samples = int(len(occ_dem_msgs) / self.unpacker.tell() * file_size)
-
-                progress_bar = Bar(f"Processing msgspack from {self.config['msgpack_path']}",
-                                   max=self.total_num_samples)
-
             for msg in zip(occ_dem_msgs, occ_data_um_msgs, gt_dem_msgs, gt_data_um_msgs):
                 occ_dem_msg, occ_data_um_msg, gt_dem_msg, gt_data_um_msg = msg
                 time = occ_dem_msg["time"]
                 h, w = occ_dem_msg["height"], occ_dem_msg["width"]
-
-                res_grid = np.array([0.04, 0.04])  # TODO
-                rel_position = np.array([0, 0, 0])  # TODO
-                rel_attitude = Rotation.from_euler('zyx', [0, 0, 0]).as_quat()
 
                 occ_dem = np.array(occ_dem_msg["data"])
                 occ_dem = occ_dem.reshape((-1, int(np.sqrt(occ_dem.shape[0]))), order="F")
@@ -88,61 +74,130 @@ class GASlamMsgpackDatasetGenerator(BaseDatasetGenerator):
                 gt_data_um = np.array(gt_data_um_msg["data"])
                 gt_data_um = gt_data_um.reshape((-1, int(np.sqrt(gt_data_um.shape[0]))), order="F")
 
-                if np.isnan(occ_dem).all():
-                    # we skip because the DEM only contains occlusion (NaNs)
-                    progress_bar.next()
-                    continue
+                res_grid = np.array([0.05, 0.05])
+                rel_position_z = occ_dem[int(occ_dem.shape[0] // 2), int(occ_dem.shape[1] // 2)]
+                rel_position = np.array([0, 0, rel_position_z])
+                rel_attitude = Rotation.from_euler('zyx', [0, 0, 0]).as_quat()
 
-                if np.isnan(gt_dem).all():
-                    # we skip because the DEM only contains missing values (NaNs)
-                    progress_bar.next()
-                    continue
+                # self.visualize(sample_idx=sample_idx, res_grid=res_grid, rel_position=rel_position,
+                #                occ_dem=occ_dem, gt_dem=gt_dem, occ_data_um=occ_data_um, gt_data_um=gt_data_um)
 
-                if prior_occ_dem is not None:
-                    # we compute MSE and PSNR between the current occluded dem and the occluded dem from the prior timestamp
-                    occ_dem_no_nan = np.nan_to_num(occ_dem, copy=True, nan=0.0)
-                    prior_occ_dem_no_nan = np.nan_to_num(prior_occ_dem, copy=True, nan=0.0)
-                    mse = mse_loss_fct(input=torch.tensor(occ_dem_no_nan),
-                                       target=torch.tensor(prior_occ_dem_no_nan))
-                    psnr = psnr_from_mse_loss_fct(mse=mse,
-                                                  data_min=np.min([occ_dem_no_nan, prior_occ_dem_no_nan]).item(),
-                                                  data_max=np.max([occ_dem_no_nan, prior_occ_dem_no_nan]).item())
+                target_size_x = self.config.get("size", occ_dem.shape[0])
+                target_size_y = self.config.get("size", occ_dem.shape[1])
+                num_subgrids_x = int(np.floor(occ_dem.shape[0] / target_size_x))
+                num_subgrids_y = int(np.floor(occ_dem.shape[1] / target_size_y))
 
-                    # we want to exclude dems which are too similar
-                    if psnr > self.config.get("psnr_similarity_threshold", 50):
+                assert num_subgrids_x >= 1 and num_subgrids_y >= 1
+
+                if progress_bar is None:
+                    # we extrapolate the total maximum number of samples
+                    # by comparing the number of messages and size of the current chunk
+                    # TODO: I am not sure if this code is correct (for multiple chunks)
+                    file_size = os.path.getsize(self.config["msgpack_path"])  # in bytes
+                    self.total_num_samples = int(len(occ_dem_msgs) / self.unpacker.tell() * file_size)
+                    self.total_num_samples *= num_subgrids_x * num_subgrids_y  # multiply with the number of subgrids
+
+                    progress_bar = Bar(f"Processing msgspack from {self.config['msgpack_path']}",
+                                       max=self.total_num_samples)
+
+                start_x = 0
+                for i in range(num_subgrids_x):
+                    stop_x = start_x + target_size_x
+                    start_y = 0
+                    for j in range(num_subgrids_y):
+                        stop_y = start_y + target_size_y
+
+                        occ_dem_subgrid = occ_dem[start_x:stop_x, start_y:stop_y]
+                        occ_data_um_subgrid = occ_data_um[start_x:stop_x, start_y:stop_y]
+                        gt_dem_subgrid = gt_dem[start_x:stop_x, start_y:stop_y]
+                        gt_data_um_subgrid = gt_data_um[start_x:stop_x, start_y:stop_y]
+
+                        subgrid_delta_x = res_grid[0] * (-occ_dem.shape[0] / 2 + start_x + target_size_x / 2)
+                        subgrid_delta_y = res_grid[1] * (-occ_dem.shape[1] / 2 + start_y + target_size_y / 2)
+                        rel_position_subgrid_z = occ_dem_subgrid[int(target_size_x//2), int(target_size_y//2)]
+                        rel_position_subgrid = np.array([rel_position[0] + subgrid_delta_x,
+                                                         rel_position[1] + subgrid_delta_y,
+                                                         rel_position_subgrid_z])
+
+                        if np.isnan(occ_dem_subgrid).all():
+                            # we skip because the DEM only contains occlusion (NaNs)
+                            start_y = stop_y
+                            progress_bar.next()
+                            continue
+
+                        if np.isnan(gt_dem_subgrid).all():
+                            # we skip because the DEM only contains missing values (NaNs)
+                            pass
+                            # start_y = stop_y
+                            # progress_bar.next()
+                            # continue
+
+                        if np.isnan(occ_dem_subgrid).sum() > (target_size_x * target_size_y / 2):
+                            # we do not want to include the subgrid in the dataset if its occluded to more than 50%
+                            start_y = stop_y
+                            progress_bar.next()
+                            continue
+
+                        if prior_occ_dem is not None:
+                            # we compute MSE and PSNR between the current occluded dem and the occluded dem from the prior timestamp
+                            prior_occ_dem_subgrid = prior_occ_dem[start_x:stop_x, start_y:stop_y]
+
+                            occ_dem_subgrid_no_nan = np.nan_to_num(occ_dem_subgrid, copy=True, nan=0.0)
+                            prior_occ_dem_subgrid_no_nan = np.nan_to_num(prior_occ_dem_subgrid, copy=True, nan=0.0)
+
+                            mse = mse_loss_fct(input=torch.tensor(occ_dem_subgrid_no_nan),
+                                               target=torch.tensor(prior_occ_dem_subgrid_no_nan))
+
+                            data_min = np.min([occ_dem_subgrid_no_nan, prior_occ_dem_subgrid_no_nan]).item()
+                            data_max = np.max([occ_dem_subgrid_no_nan, prior_occ_dem_subgrid_no_nan]).item()
+                            psnr = psnr_from_mse_loss_fct(mse=mse,
+                                                          data_min=data_min,
+                                                          data_max=data_max)
+
+                            # we want to exclude dems which are too similar
+                            if psnr > self.config.get("psnr_similarity_threshold", 50):
+                                start_y = stop_y
+                                progress_bar.next()
+                                continue
+
+                        self.res_grid.append(res_grid)
+                        self.rel_positions.append(rel_position_subgrid)
+                        self.rel_attitudes.append(rel_attitude)
+                        self.occ_dems.append(occ_dem_subgrid)
+                        self.occ_data_ums.append(occ_data_um_subgrid)
+                        self.gt_dems.append(gt_dem_subgrid)
+
+                        if self.initialized_datasets is False:
+                            super().create_base_datasets(self.hdf5_group, self.total_num_samples)
+
+                            self.hdf5_group.create_dataset(name=ChannelEnum.OCC_DEM.value,
+                                                           shape=(0, occ_dem_subgrid.shape[0], occ_dem_subgrid.shape[1]),
+                                                           maxshape=(self.total_num_samples,
+                                                           occ_dem_subgrid.shape[0], occ_dem_subgrid.shape[1]))
+                            self.hdf5_group.create_dataset(name=ChannelEnum.OCC_DATA_UM.value,
+                                                           shape=(0, occ_data_um_subgrid.shape[0],
+                                                                  occ_data_um_subgrid.shape[1]),
+                                                           maxshape=(self.total_num_samples,
+                                                                     occ_data_um_subgrid.shape[0],
+                                                                     occ_data_um_subgrid.shape[1]))
+                            self.hdf5_group.create_dataset(name=ChannelEnum.GT_DEM.value,
+                                                           shape=(0, gt_dem_subgrid.shape[0], gt_dem_subgrid.shape[1]),
+                                                           maxshape=(self.total_num_samples,
+                                                                     gt_dem_subgrid.shape[0], gt_dem_subgrid.shape[1]))
+
+                        if self.sample_idx % self.config.get("save_frequency", 50) == 0:
+                            self.save_cache()
+
+                        self.visualize(sample_idx=sample_idx, res_grid=res_grid, rel_position=rel_position_subgrid,
+                                       occ_dem=occ_dem_subgrid, gt_dem=gt_dem_subgrid,
+                                       occ_data_um=occ_data_um_subgrid, gt_data_um=gt_data_um_subgrid)
+
+                        prior_occ_dem = occ_dem
+                        sample_idx += 1
+                        start_y = stop_y
                         progress_bar.next()
-                        continue
 
-                self.res_grid.append(res_grid)
-                self.rel_positions.append(rel_position)
-                self.rel_attitudes.append(rel_attitude)
-                self.occ_dems.append(occ_dem)
-                self.occ_data_ums.append(occ_data_um)
-                self.gt_dems.append(gt_dem)
-
-                if self.initialized_datasets is False:
-                    super().create_base_datasets(self.hdf5_group, self.total_num_samples)
-
-                    self.hdf5_group.create_dataset(name=ChannelEnum.OCC_DEM.value,
-                                                   shape=(0, occ_dem.shape[0], occ_dem.shape[1]),
-                                                   maxshape=(self.total_num_samples, occ_dem.shape[0], occ_dem.shape[1]))
-                    self.hdf5_group.create_dataset(name=ChannelEnum.OCC_DATA_UM.value,
-                                                   shape=(0, occ_data_um.shape[0], occ_data_um.shape[1]),
-                                                   maxshape=(self.total_num_samples, occ_data_um.shape[0],
-                                                             occ_data_um.shape[1]))
-                    self.hdf5_group.create_dataset(name=ChannelEnum.GT_DEM.value,
-                                                   shape=(0, gt_dem.shape[0], gt_dem.shape[1]),
-                                                   maxshape=(self.total_num_samples, gt_dem.shape[0], gt_dem.shape[1]))
-
-                if self.sample_idx % self.config.get("save_frequency", 50) == 0:
-                    self.save_cache()
-
-                self.visualize(sample_idx=sample_idx, res_grid=res_grid, rel_position=rel_position,
-                               occ_dem=occ_dem, gt_dem=gt_dem, occ_data_um=occ_data_um, gt_data_um=gt_data_um)
-
-                prior_occ_dem = occ_dem
-                sample_idx += 1
-                progress_bar.next()
+                    start_x = stop_x
 
         self.save_cache()
         progress_bar.finish()
